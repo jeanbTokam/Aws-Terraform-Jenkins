657189437a66857c2d6072ab523a589fbc901df9
jbtokam <85589835+jeanbTokam@users.noreply.github.com>
2023-02-11 01:09:53 -0500

    Updated terraform file

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
 			provider "aws" {
 			  region = "us-east-2"
-			  #   access_key = ""
-			  #   secret_key = ""	
+	
 			}
 			
 			resource "aws_instance" "intro" {
 			  ami                    = "ami-0aa7d40eeae50c9a9"
 			  instance_type           = "t2.micro"
-			  availability_zone      = "us-east-2a"
-			  key_name               = "tf"
 			  vpc_security_group_ids = ["sg-02b30ba6390840314"]
 			  tags = {
 			    Name    = "TF-CICD"
