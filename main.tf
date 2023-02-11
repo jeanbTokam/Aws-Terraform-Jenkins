2aa97d403997cce6d1f32d27a2aac62bb4c321e4
jbtokam <85589835+jeanbTokam@users.noreply.github.com>
2023-02-11 02:06:34 -0500

    Edited main terra

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 			provider "aws" {
-			  region = "us-east-2"
+			  region = "us-east-1"
 	
 			}
 			
 			resource "aws_instance" "intro" {
 			  ami                    = "ami-0aa7d40eeae50c9a9"
 			  instance_type           = "t2.micro"
-			  vpc_security_group_ids = ["sg-02b30ba6390840314"]
+			  vpc_security_group_ids = ["sg-093c1f7fe381c37b0"]
 			  tags = {
-			    Name    = "TF-CICD"
-			    Project = "AWS-TERRAFORM-JENKINS"
+			    Name    = "tf-key"
+			  
   }
       }
